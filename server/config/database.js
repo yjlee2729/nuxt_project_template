71d4736d89942e594bdc4c2510b707b3dd560110
yejinLee <yejinLee2729@lgcns.com>
2019-08-28 14:39:57 +0900

    db config

---
+++ server/config/database.js
@@ -0,0 +1,21 @@
+module.exports = {
+    /* dev */
+    dev : {
+        host : 'host',
+        port : 'port',
+        user : 'user',
+        password : 'password',
+        database : 'dbname',
+        connectionLimit : 10,
+        connectTimeout : 100000
+    },
+    op : {
+        host : 'host',
+        port : 'port',
+        user : 'user',
+        password : 'password',
+        database : 'dbname',
+        connectionLimit : 10,
+        connectTimeout : 100000
+    }
+}
